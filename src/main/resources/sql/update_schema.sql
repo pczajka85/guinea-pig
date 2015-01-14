a7240431ba7aa1e5a6f88433c0b23d6182b56bf0
Pio <pczajka85@gmail.com>
2015-01-14 10:09:33 +0100

    first commit

---
+++ src/main/resources/sql/update_schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE "TABLE_FROM_BASH";
+
+CREATE TABLE "TABLE_FROM_BASH"
+(
+  name character varying[],
+  id bigint NOT NULL,
+  CONSTRAINT tfb_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "TABLE_FROM_BASH"
+  OWNER TO postgres;
